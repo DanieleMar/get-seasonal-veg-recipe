a64fd06402b0e9ae1aae4a4c9914e8f6f60e14a7
Dan <daniele.mariotto@gmail.com>
2020-12-09 18:14:25 +0100

    show seasonal veget

---
+++ src/components/seasonalVegetables/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SeasonalVegetables() {
+    let meseCorrente = 'Dicembre'
+    return (
+        <div>
+            Ecco le verdure di stagione a {meseCorrente} in Italia
+            AGLIO, BIETA, CARCIOFO, CARDO, CAROTA, CAVOLO, CECE, <br></br> CICORIA, CIPOLLA, ERBA CIPOLLINA, FAGIOLO, FINOCCHIO, LATTUGA, <br></br> LENTICCHIA, PATATA, PISELLO, PORRO, RADICCHIO, SCALOGNO, <br></br> SEDANO, SPINACIO, TOPINAMBUR, ZUCCA
+        </div>
+    )
+}
