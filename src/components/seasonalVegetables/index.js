2434fb7f22ad34fdd5029f2d36283d16f0fc5729
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-12-15 12:15:23 +0100

    moved from listOfRecipes

--- src/components/seasonalVegetables/index.js
+++ src/components/seasonalVegetables/index.js
@@ -1,11 +1,71 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { getRecipe } from "../../utils/fetch/get";
+import SingleRecipe from "../singleRecipe";
 export default function SeasonalVegetables() {
-    let meseCorrente = 'Dicembre'
-    return (
-        <div>
-            Ecco le verdure di stagione a {meseCorrente} in Italia
-            AGLIO, BIETA, CARCIOFO, CARDO, CAROTA, CAVOLO, CECE, <br></br> CICORIA, CIPOLLA, ERBA CIPOLLINA, FAGIOLO, FINOCCHIO, LATTUGA, <br></br> LENTICCHIA, PATATA, PISELLO, PORRO, RADICCHIO, SCALOGNO, <br></br> SEDANO, SPINACIO, TOPINAMBUR, ZUCCA
-        </div>
-    )
+  const [recipes, setRecipes] = useState([]);
+  const listOfVegetables = [
+    "Garlic",
+    "Chard",
+    "Artichoke",
+    "Thistle",
+    "Carrot",
+    "Cabbage",
+    "Chickpea",
+    "Chicory",
+    "Onion",
+    "Chives",
+    "Bean",
+    "Fennel",
+    "Lettuce",
+    "Lentil",
+    "Potato",
+    "Pea",
+    "Leek",
+    "Radicchio",
+    "Shallot",
+    "Celery",
+    "Spinach",
+    "JerusalemArtichoke",
+    "Pumpkin",
+  ];
+
+  const getData = async (value) => {
+    const data = await getRecipe(value);
+    if (data !== undefined) {
+      const { hits, count } = data;
+      getSetRecipes(hits, count);
+    }
+  };
+  const getSetRecipes = (hits, count) => {
+    //creare un array fatto di oggetti recipe che contengono nome e url della ricetta
+    //deve essere lungo, la metà degli elementi di hits
+    const recipeArray = hits.map((value, index) => {
+      const { recipe } = value;
+      const { label, url, image } = recipe;
+      return { label, url, image };
+    });
+
+    setRecipes(recipeArray);
+  };
+
+  let meseCorrente = "Dicembre";
+  return (
+    <div>
+
+      Ecco le verdure di stagione a {meseCorrente} in Italia <br></br>
+      {listOfVegetables.map((value, index) => {
+        return (
+          <span key={index} onClick={() => getData(value)}>{`${value}, `}</span>
+        );
+      })}
+
+      {recipes.length > 0 &&
+        recipes.map((recipe, index) => {
+          const { label, image, url } = recipe;
+          return (
+            <SingleRecipe key={index} name={label} url={url} imgUrl={image} />
+          );
+        })}
+    </div>
+  );
 }
