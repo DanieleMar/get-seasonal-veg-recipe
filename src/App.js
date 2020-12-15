03521865a823c90c674fdf61b926fae400ba88ef
Daniele Mariotto <daniele.mariotto@gmail.com>
2020-12-15 12:23:15 +0100

    moved from seasonalVeget component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import logo from "./logo.svg";
-import "./App.css";
-import ListOfRecipes from "./components/listOfRecipes";
-import SeasonalVegetables from "./components/seasonalVegetables";
+import React, { useState } from "react";
+import { getRecipe } from "./utils/fetch/get";
+import SingleRecipe from "./components/singleRecipe";
 
-function App() {
+export default function App() {
+  const [recipes, setRecipes] = useState([]);
+  const listOfVegetables = [
+    "Garlic",
+    "Chard",
+    "Artichoke",
+    "Thistle",
+    "Carrot",
+    "Cabbage",
+    "Chickpea",
+    "Chicory",
+    "Onion",
+    "Chives",
+    "Bean",
+    "Fennel",
+    "Lettuce",
+    "Lentil",
+    "Potato",
+    "Pea",
+    "Leek",
+    "Radicchio",
+    "Shallot",
+    "Celery",
+    "Spinach",
+    "JerusalemArtichoke",
+    "Pumpkin",
+  ];
 
-  
+  const getData = async (value) => {
+    const data = await getRecipe(value);
+    if (data !== undefined) {
+      const { hits, count } = data;
+      getSetRecipes(hits, count);
+    }
+  };
+  const getSetRecipes = (hits, count) => {
+    //creare un array fatto di oggetti recipe che contengono nome e url della ricetta
+    //deve essere lungo, la metà degli elementi di hits
+    const recipeArray = hits.map((value, index) => {
+      const { recipe } = value;
+      const { label, url, image } = recipe;
+      return { label, url, image };
+    });
+
+    setRecipes(recipeArray);
+  };
+
+  let meseCorrente = "Dicembre";
   return (
-    <div className="App">
-      <SeasonalVegetables />
-      <ListOfRecipes />
+    <div>
+
+      Ecco le verdure di stagione a {meseCorrente} in Italia <br></br>
+      {listOfVegetables.map((value, index) => {
+        return (
+          <span key={index} onClick={() => getData(value)}>{`${value}, `}</span>
+        );
+      })}
+
+      {recipes.length > 0 &&
+        recipes.map((recipe, index) => {
+          const { label, image, url } = recipe;
+          return (
+            <SingleRecipe key={index} name={label} url={url} imgUrl={image} />
+          );
+        })}
     </div>
   );
 }
 
-export default App;
